5802b77215a77bfca00fe37b277a62e812ef3210
nomun munkhbaatar <ninjanomun@gmail.com>
2022-10-24 23:12:23 +0800

    nomun tsarailag

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    opacity: {
+      0: "0",
+      10: ".1",
+      20: ".2",
+      30: ".3",
+      40: ".4",
+      50: ".5",
+      60: ".6",
+      70: ".7",
+      80: ".8",
+      90: ".9",
+      100: "1",
+      5: ".05",
+      25: ".25",
+      75: ".75",
+    },
+    extend: {
+      colors: {
+        "primary-blue": "#1992d4",
+      },
+      spacing: {
+        72: "18rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["responsive", "group-hover"],
+      backgroundColor: ["responsive", "hover", "focus", "active"],
+      // ...
+      borderColor: ["focus-visible", "first"],
+      // ...
+      textColor: ["visited"],
+    },
+  },
+  plugins: [],
+};
+
